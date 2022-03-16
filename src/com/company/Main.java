ea664edd5f2b5f5063e70a7a16ea52bf5116f4f6
KALYS'S LAPTOP <bekmenbetov@gmail.com>
2022-03-16 21:18:25 +0600

    tw commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         cars.put(new Car(1134,607),new Data(2012,"Toyota Camry",12000,"white "));
         cars.put(new Car(4234,408),new Data(2011,"Toyota Opa",3700,"red "));
         cars.put(new Car(4634,513),new Data(2000,"Honda Accord",3800,"black "));
-        
+
         //System.out.println(cars.entrySet());
         for (Map.Entry elem: cars.entrySet()
              ) {
